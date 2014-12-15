4ac9e6c98b14933882ddac38f1d2e335ae4aca69
Alban Mouton <mouton-a@mgdis.fr>
2014-12-15 13:37:38 +0100

    only write mock file if some requests were actually recorded

--- test/root.js
+++ test/root.js
@@ -1,9 +1,12 @@
-// Some global before and after test processing
+// Some global before and after for the test suite
 
 var fs = require('fs');
 var nock = require('nock');
 
 if (process.env.NOCK_OFF && process.env.NOCK_RECORD) {
+	// If HTTP mocks using Nock are disabled and recording is asked for using environement variables
+	// then record a mock file for each individual test of the suite
+
 	before(function() {
 		nock.recorder.rec({
 			output_objects: true,
@@ -16,15 +19,19 @@ if (process.env.NOCK_OFF && process.env.NOCK_RECORD) {
 	afterEach(function() {
 		var nockCalls = nock.recorder.play();
 		var mockFilePath = './test/mocks/' + this.currentTest.title.replace(/\s/g, '_') + '.json';
-		fs.writeFileSync(mockFilePath, JSON.stringify(nockCalls, null, 4));
 		if (nockCalls && nockCalls.length > 0) {
+			fs.writeFileSync(mockFilePath, JSON.stringify(nockCalls, null, 4));
 			console.log('Nock recorded some requests and wrote to "%s"', mockFilePath);
 		}
 	});
-} else if (!process.env.NOCK_OFF){
+} else if (!process.env.NOCK_OFF) {
+	// If HTTP mocks using Nock are active the load o mock file for each individual test
+
 	beforeEach(function() {
 		var mockFilePath = './test/mocks/' + this.currentTest.title.replace(/\s/g, '_') + '.json';
-		console.log('Loading mocks from "%s"', mockFilePath);
-		nocks = nock.load(mockFilePath);
+		if (fs.existsSync(mockFilePath)) {
+			console.log('Loading mocks from "%s"', mockFilePath);
+			nock.load(mockFilePath);
+		}
 	});
 }
